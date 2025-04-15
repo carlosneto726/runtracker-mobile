087da1474f97b6c20fb4352cada96b8997ed318e
carlosneto726 <carlosneto726@gmail.com>
2025-04-15 11:25:40 -0300

    feat: :recycle: adiciona paginas e links para o navigator

--- index.tsx
+++ index.tsx
@@ -3,21 +3,53 @@ import { AppRegistry } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { name as appName } from './app.json';
-
-import { HomeScreen } from './src/screens/Home';
-import { Login } from './src/screens/Login';
-import { Register } from './src/screens/Register';
-import { Lap } from './src/screens/Lap';
+import { HomeScreen, Login, Register, Lap, Teste, Circuit, CircuitStore, Profile } from './src/screens';
 
 const Stack = createNativeStackNavigator();
+const CircuitStack = createNativeStackNavigator();
+
+const linking = {
+    prefixes: ['runTracker://'],
+    config: {
+        screens: {
+            Background: {
+                path: 'Background'
+            },
+            Home: {
+                path: 'Home'
+            },
+            Login: {
+                path: 'Login'
+            },
+            Registrar: {
+                path: 'Registrar'
+            },
+            Lap: {
+                path: 'Lap'
+            },
+        }
+    }
+}
 
 function ExampleApp() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+        linking={ linking }
+        fallback={<></>}>
       <Stack.Navigator>
+        {/* <Stack.Screen name="Teste" component={Teste} /> */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Registrar" component={Register} />
+        <Stack.Screen name="Profile" component={Profile} />
+
+        <Stack.Screen name="Circuit" children={() =>
+            <CircuitStack.Navigator>
+                <CircuitStack.Screen name="Dashboard" component={Circuit}/>
+                <CircuitStack.Screen name="Store" component={CircuitStore}/>
+            </CircuitStack.Navigator>
+        }/>
+
         <Stack.Screen name="Lap" component={Lap} />
       </Stack.Navigator>
     </NavigationContainer>
